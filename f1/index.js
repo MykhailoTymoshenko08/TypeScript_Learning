89cb32696359f991c64817a7ff2f7ab40252c37b
Misha <113121252+MykhailoTymoshenko08@users.noreply.github.com>
2026-01-30 23:49:22 +0200

    Add files via upload

---
+++ f1/index.js
@@ -0,0 +1,2 @@
+console.log('This is f1/index.ts');
+console.log("Hello, TypeScript!");
---
+++ f1/index.ts
@@ -0,0 +1,20 @@
+let age = 30
+let age2: number = 30
+
+console.log(age, age2);
+
+
+let arrays: number[] = [1,2]
+arrays.push(3)
+let things = [1, true, "dd"]
+console.log(things[1])
+
+
+let user:   {
+    first_name: string,
+    age: number
+} = {
+    first_name: "LAb",
+    age: 30
+}
+user.age = 12
