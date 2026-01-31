3243813fb32849d65a1bc9dc67b7bf55dd6e6d46
Misha <113121252+MykhailoTymoshenko08@users.noreply.github.com>
2026-01-31 19:33:48 +0200

    Delete folder2 directory

--- folder2/index.ts
+++
@@ -1,8 +0,0 @@
-let projectName: string;
-let modelsCount: number;
-let isOpenSource: boolean;
-let supportedLanguages: string[] = ["English", "Spanish", "Ukrainian"]
-
-function getProjectStatus(name_of_projecct :string, count: number) :string{
-    return `Project ${name_of_projecct} work with ${count} models`;
-}
